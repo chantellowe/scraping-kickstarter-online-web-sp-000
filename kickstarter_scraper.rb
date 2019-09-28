ef95afeeec6346609e3ce41dc65768cf98e6af6e
Chantel Lowe <chantellowe17@gmail.com>
2019-09-28 16:13:41 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,8 +8,8 @@ def create_project_hash
   html = File.read("fixtures/kickstarter.html")
   
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
-end
+  #binding.pry
+
 
 #projects: kickstarter.css("li.project.grid_4")
 #title: project.css("h2.bbcard_name strong a").text
@@ -17,5 +17,9 @@ end
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("span.location-name").text
 #percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
+  projects
+
+end
 
 create_project_hash
